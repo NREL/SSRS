a1a649db48a7cdcaa7a4e12f1f3e294af3e2c3e1
Eliot Quon <eliot.quon@nrel.gov>
2022-01-25 14:13:30 -0700

    Update example docstrings

--- examples/example_jem.py
+++ examples/example_jem.py
@@ -1,13 +1,13 @@
-""" Scipt for running the SSRS simulation in Wyoming
-around Top of the World wind power plane at 50 m resolution"""
+"""Script for running the SSRS simulation in Wyoming
+around Top of the World wind power plant at 50-m resolution"""
 
 from dataclasses import replace
 from ssrs import Simulator, Config
 
 config_jem_base = Config(
     run_name='jem_wy',
-    max_cores=16,
-    out_dir='./output',
+    max_cores=4, # 16
+    out_dir='./ref_output',
     southwest_lonlat=(-106.21, 42.78),  # (lon, lat) for southwest pt
     region_width_km=(50., 50.),  # terrain width (xwidth, ywidth) in km
     resolution=50.,

83de1ca0e31de81f1e31cd9a43f2adaf45379b6a
Eliot Quon <eliot.quon@nrel.gov>
2023-03-21 14:14:58 -0600

    Update script

--- tests/save_gold_files.sh
+++ tests/save_gold_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-cp TOTW_case1_2020-03-18_12Z/data/snapshot/y2020m03d18h12_d180_t0_fluid-flow_r0_tracks.pkl gold_files/TOTW_case1_2020-03-18_12Z/
-cp TOTW_case2_2020-03-04_13Z/data/snapshot/y2020m03d04h13_d135_t0_fluid-flow_r0_tracks.pkl gold_files/TOTW_case2_2020-03-04_13Z/
-cp TOTW_case3_2019-07-03_12Z/data/snapshot/y2019m07d03h12_d0_t0_fluid-flow_r0_tracks.pkl gold_files/TOTW_case3_2019-07-03_12Z/
-cp TOTW_case4_2020-04-18_10Z/data/snapshot/y2020m04d18h10_d270_t0_fluid-flow_r0_tracks.pkl gold_files/TOTW_case4_2020-04-18_10Z/
+cp TOTW_case1_2020-03-18_12Z/data/snapshot/*_tracks.pkl gold_files/TOTW_case1_2020-03-18_12Z/
+cp TOTW_case2_2020-03-04_13Z/data/snapshot/*_tracks.pkl gold_files/TOTW_case2_2020-03-04_13Z/
+cp TOTW_case3_2019-07-03_12Z/data/snapshot/*_tracks.pkl gold_files/TOTW_case3_2019-07-03_12Z/
+cp TOTW_case4_2020-04-18_10Z/data/snapshot/*_tracks.pkl gold_files/TOTW_case4_2020-04-18_10Z/
